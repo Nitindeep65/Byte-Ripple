7404f80cb5345492aa0bf97b218dafe463615a24
Nitindeep Singh <nittin@Nitindeeps-MacBook-Air.local>
2025-08-14 16:33:07 +0530

    commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1>Welcome to ByteRipple</h1>
+      <p>Your one-stop solution for all things tech.</p>
+    </div>
+  )
+}
+
+export default page
